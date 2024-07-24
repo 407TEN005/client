23dce436eed5d9aab6210385bf52780b21d472a7
yechan <kevin2695@naver.com>
2024-07-24 22:09:14 +0900

    [TASK-164] react-day-picker 적용

--- src/pages/CreateTravel/CreateTravel.tsx
+++ src/pages/CreateTravel/CreateTravel.tsx
@@ -1,25 +1,69 @@
 import styles from './CreateTravel.module.scss';
 import Input from '@components/Input';
+import useInput from '@hooks/useInput';
 
-// todo : 일정 라이브러리 찾아보기
+import { format } from 'date-fns';
+
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+
+const TRAVEL_NAME_PLACEHOLDER = 'ex. 우리 가족 첫 일본 여행';
+const TRAVEL_NAME_ERROR_MESSAGE = '최대 20자까지 입력 가능해요';
 
 const CreateTravel = () => {
+  const {
+    data: name,
+    errorMessage: nameErrorMessage,
+    handleChange: handleNameChange,
+  } = useInput(TRAVEL_NAME_ERROR_MESSAGE);
+
   return (
     <div className={styles.wrapper}>
       <Input
+        id="travelName"
         label="어떤 여행인가요?"
+        value={name}
+        errorMessage={nameErrorMessage}
+        placeholder={TRAVEL_NAME_PLACEHOLDER}
         isRequired
-        // isError
-        ErrorMessage="에러메시지에러메시지에러메시지"
-        placeholder="ex. 우리 가족 첫 일본 여행"
+        isError={Boolean(nameErrorMessage)}
+        onChange={(e) => handleNameChange(e.target.value)}
       />
-      <Input
+      {/* <Input
         label="함께하는 가족은 몇 명인가요?"
         isRequired
         // isError
         ErrorMessage="에러메시지에러메시지에러메시지"
         placeholder="ex. 4"
-      />
+      /> */}
+      <label className={styles.label}>
+        여행 일정을 입력해 주세요. <span>*</span>
+      </label>
+      <div className={styles.dayPickerWrapper}>
+        <DayPickerInput
+          dayPickerProps={{
+            locale: 'ko',
+          }}
+          inputProps={{
+            className: `${styles.input} ${styles.dayPickerInput}`,
+          }}
+          onDayChange={(day) => console.log(day)}
+          placeholder={`${format(new Date(), 'yyyy-MM-dd')}`}
+        />
+        <p> ~ </p>
+        <DayPickerInput
+          inputProps={{
+            className: `${styles.input} ${styles.dayPickerInput}`,
+          }}
+          onDayChange={(day) => console.log(day)}
+          classNames={{
+            overlayWrapper: styles.dayPicker,
+            container: '',
+            overlay: '',
+          }}
+          placeholder={`${format(new Date(), 'yyyy-MM-dd')}`}
+        />
+      </div>
     </div>
   );
 };
