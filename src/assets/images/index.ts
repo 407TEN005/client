336c9d54ebce6b62811e9a325b0d7d014910b578
yechan <kevin2695@naver.com>
2024-07-26 01:20:18 +0900

    [TASK-172] svg 이미지 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,1 +1,11 @@
 export { ReactComponent as TestLogo } from './test_logo.svg';
+
+export { ReactComponent as TestFather } from './test_father.svg';
+export { ReactComponent as TestMother } from './test_mother.svg';
+export { ReactComponent as TestSon } from './test_son.svg';
+export { ReactComponent as TestDaughter } from './test_daughter.svg';
+
+export { ReactComponent as SelectedTestFather } from './selected_test_father.svg';
+export { ReactComponent as SelectedTestMother } from './selected_test_mother.svg';
+export { ReactComponent as SelectedTestSon } from './selected_test_son.svg';
+export { ReactComponent as SelectedTestDaughter } from './selected_test_daughter.svg';
