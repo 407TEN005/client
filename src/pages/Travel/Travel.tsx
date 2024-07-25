1a33b06bb1a41e9d4a31a432e8d2acbd2e9fa194
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-07-26 06:54:16 +0900

    [TASK-174] 타이포그래피 caption 수정, 추가

--- src/pages/Travel/Travel.tsx
+++ src/pages/Travel/Travel.tsx
@@ -1,5 +1,23 @@
+import styles from './Travel.module.scss';
+
 const Travel = () => {
-  return <div>여행 방 페이지 입니다. </div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.titleContainer}>
+        <div className={styles.dday}>D-17</div>
+        <div className={styles.title}>온가족이 함께 하는 방콕 휴가</div>
+        <div className={styles.travelDate}>2024.08.12 - 2024.08.20</div>
+      </div>
+      <div>
+        <div>함께하는 우리 가족</div>
+        <div></div>
+      </div>
+      <div className={styles.commandmentList}>
+        <div>이번 여행의 10계명</div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Travel;
