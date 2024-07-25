ac7b90a09bb0b6167bda678e4b1680164316e0b4
yechan <kevin2695@naver.com>
2024-07-25 22:08:07 +0900

    [TASK-172] svg 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import svgr from '@svgr/rollup';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), svgr(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@import "./src/assets/styles/utils.scss";`,
       },
     },
+  },
+  resolve: {
+    alias: [{ find: '@images', replacement: path.resolve(__dirname, 'src/assets/images') }],
   },
 });
