51c7073d349adc553f0ba5422fe600abe3b2f466
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-07-26 13:23:38 +0900

    [TASK-176] 버튼 컴포넌트 gray 추가

--- src/pages/TestResult/TestResult.tsx
+++ src/pages/TestResult/TestResult.tsx
@@ -1,9 +1,25 @@
 import styles from './TestResult.module.scss';
+import Button from '@src/components/Button';
+import Sample from '@src/assets/images/testresultimg.svg';
 
 const TestResult = () => {
   return (
     <div className={styles.wrapper}>
-      <h1>Test Result</h1>
+      <div className={styles.subTitle}>나의 여행 성향은</div>
+      <div className={styles.title}>열정 넘치는 불꽃 부모</div>
+      <div className={styles.tagsWrapper}>
+        <div className={styles.tag}>#도전은내운명</div>
+        <div className={styles.tag}>#도전은내운명</div>
+        <div className={styles.tag}>#즉흥여행의달인</div>
+      </div>
+      <div className={styles.characterImg}>
+        <img src={Sample} />
+      </div>
+      <div className={styles.description}></div>
+      <div className={styles.description}></div>
+      <Button variant="gray" size="xl">
+        텐텐 시작하기
+      </Button>
     </div>
   );
 };
