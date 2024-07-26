63544949b1db9833b783eecaebb14939c6ee2316
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-07-26 11:45:49 +0900

    [TASK-176] lottie-react 설치, 로딩스피너, 샘플이미지 추가

--- src/pages/TestResult/TestResult.tsx
+++ src/pages/TestResult/TestResult.tsx
@@ -1,6 +1,8 @@
+import styles from './TestResult.module.scss';
+
 const TestResult = () => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <h1>Test Result</h1>
     </div>
   );
