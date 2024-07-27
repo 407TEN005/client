1c90863a8608ed9bc075cf5182bf2aea5d158f69
yechan <kevin2695@naver.com>
2024-07-27 22:26:16 +0900

    [TASK-187] header 설정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
-import Button from '../Button';
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
+import { Exit, LeftArrow } from '@images/index';
 
 const Header = ({ onBack }: { onBack: () => void }) => {
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <Button variant="outlined" size="m" onClick={onBack}>
-        1
-      </Button>
-      <Button variant="outlined" size="m">
-        2
-      </Button>
+      <div onClick={onBack}>
+        <LeftArrow />
+      </div>
+      <div onClick={handleExit}>
+        <Exit />
+      </div>
     </div>
   );
 };
