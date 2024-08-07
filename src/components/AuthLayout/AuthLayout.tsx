44f2ee3b25343d6230ae2abc882c7a7da4655fcb
yechan <kevin2695@naver.com>
2024-08-07 21:47:03 +0900

    [TASK-200] console 확인을 위한 커밋

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -1,16 +1,22 @@
 import authUtil from '@utils/authUtil';
-import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate, useParams } from 'react-router-dom';
 import ROUTES from '@constants/routes';
 import { useEffect } from 'react';
 
 const AuthLayout = ({ shouldProtect }: { shouldProtect: boolean }) => {
   const navigate = useNavigate();
 
+  const { pathname } = useParams();
+
+  console.log('pathname : ', pathname);
+
   const hasAuth = authUtil.isAuth();
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const roomId = params.get('roomId');
+
+    console.log('roomId : ', roomId);
 
     if (roomId) {
       authUtil.setRoomId({ roomId });
